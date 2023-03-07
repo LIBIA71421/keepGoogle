b9f4b8dbdf6c6e863b2f1b5d3b1475fef05fab03
libia <libia.roman@ucb.edu.bo>
2023-03-07 06:33:58 +0100

    AlmacenarNuevaNota

--- src/crearNota.js
+++ src/crearNota.js
@@ -1,8 +1,22 @@
 document.getElementById('crear-nota-form').addEventListener('submit',guardarNota);
 
 function guardarNota(e){
+    //guarda valores
     let titulo = document.getElementById('titulo').value;
     let descripcion = document.getElementById('descripcion').value;
 
+    //crea la nota como un objeto
+    const Nota = {
+        titulo,
+        descripcion
+    };
+
+    //usa el storage para almacenar las notas cuando no hay ninguna
+    if(localStorage.getItem('notas') == null){
+        let notas = [];
+        notas.push(Nota);
+        localStorage.setItem('notas', JSON.stringify(notas));
+    }
+    
     e.preventDefault();
 }
