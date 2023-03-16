1db8a7e514d142dc96b819cf979e8a1aa8ad66fe
libia <libia.roman@ucb.edu.bo>
2023-03-16 16:14:20 +0100

    ARR

--- src/busqueda.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('keyup',e=>{
-    if(e.target.matches("#Buscador")){
-        document.query
-    }
-})
--- src/crearNota.js
+++ src/crearNota.js
@@ -1,12 +1,12 @@
-document.getElementById('crear-nota-form').addEventListener('submit',guardarNota);
+document.getElementById('formNotas').addEventListener('submit',guardarnota);
 
-function guardarNota(e){
+function guardarnota(e){
     //guarda valores
     let titulo = document.getElementById('titulo').value;
     let descripcion = document.getElementById('descripcion').value;
 
     //crea la nota como un objeto
-    const Nota = {
+    const nota = {
         titulo,
         descripcion
     };
@@ -15,52 +15,54 @@ function guardarNota(e){
     // push - ingresar dato al arreglo
     if(localStorage.getItem('notas') == null){
         let notas = [];
-        notas.push(Nota);
+        notas.push(nota);
         localStorage.setItem('notas', JSON.stringify(notas));
     }else{ //recuperar notas y actualizarlas
         let notas = JSON.parse(localStorage.getItem('notas'));
-        notas.push(Nota);
+        notas.push(nota);
         localStorage.setItem('notas', JSON.stringify(notas));
     }
 
-    mostrarNotas();
+    mostrarnotas();
+    document.getElementById('formNotas').reset();
     e.preventDefault();
 }
 
-function eliminarNota(titulo){
+function eliminarnota(titulo){
     let notas = JSON.parse(localStorage.getItem('notas'));
     //titulos notas almacenadas splice-quitar
     for(let i=0; i<notas.length; i++){
         if(notas[i].titulo == titulo){
-            notas.splice(i,1);
+            notas.splice(i, 1);
         }
-    }
+    }        
     localStorage.setItem('notas', JSON.stringify(notas));
-    mostrarNotas();
+    mostrarnotas();
 }
 
-function mostrarNotas(){
+function mostrarnotas(){
     //mostramos en pantalla las notas desde el locaStorage
     let notas = JSON.parse(localStorage.getItem('notas'));
-    let verNotas = document.getElementById('resultado-divNota');
+    let vernotas = document.getElementById('notas');
 
-    verNotas.innerHTML = '';    //limpiamos html
+    vernotas.innerHTML = '';    //limpiamos html
 
     for(let i = 0; i < notas.length; i++){
         //recorrer todas las notas para mostrarlas
         let titulo = notas[i].titulo;
         let descripcion = notas[i].descripcion;
 
-        verNotas.innerHTML += `<div>
-            <h2>Notas: </h2>
-            <p>${titulo} - ${descripcion}</p>
-            <button onclick="eliminarNota('${titulo}')">
-                Eliminar
-            </button >
-        </div>`
+        vernotas.innerHTML += `<div class="card mb-3">
+            <div class="card-body">
+                <h2>notas: </h2>
+                <p>${titulo} - ${descripcion}
+                <a href="#" onclick="eliminarnota('${titulo}')" class="btn btn danger ml-5">
+                    Eliminar
+                </a>
+                </p>
+            </div>
+        </div>`;
     }
 }
 
-
-
-mostrarNotas();
+mostrarnotas();
